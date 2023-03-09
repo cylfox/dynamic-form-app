8aec019640687c62a1473078bb6d3ae126748d95
seroma <marcos.sep.ro@gmail.com>
2023-03-09 11:38:39 +0100

    Fixed some warnings errors

--- src/components/Form/DatePeriod.tsx
+++ src/components/Form/DatePeriod.tsx
@@ -1,7 +1,58 @@
-export default function DatePeriod(): JSX.Element {
+import type React from 'react'
+
+interface DatePeriodProps {
+  formik: {
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    values: {
+      start: string
+      end: string
+      increment: string
+    }
+    errors: Record<string, string>
+  }
+}
+
+const DatePeriod: React.FC<DatePeriodProps> = ({ formik }) => {
   return (
-    <div>
-      <p>Period</p>
-    </div>
+    <>
+      <div className="period">
+        <div>
+          <b>Period *</b>
+        </div>
+        <div>
+          <label htmlFor="start">Start</label>
+          <input
+            id="start"
+            name="start"
+            type="date"
+            onChange={formik.handleChange}
+            value={formik.values.start}
+          />
+        </div>
+        <div>
+          <label htmlFor="end">End</label>
+          <input
+            id="end"
+            name="end"
+            type="date"
+            onChange={formik.handleChange}
+            value={formik.values.end}
+          />
+        </div>
+        <div>
+          <label htmlFor="increment">Increment</label>
+          <input
+            id="increment"
+            name="increment"
+            type="number"
+            onChange={formik.handleChange}
+            value={formik.values.increment}
+          />
+        </div>
+      </div>
+      <p>{Object.values(formik.errors).join(' ')}</p>
+    </>
   )
 }
+
+export default DatePeriod
